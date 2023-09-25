d0ec0bdb4aa0f2a45442d4bd35f68cb741c12337
Valentisa11 <valya.kobylina@yandex.ru>
2023-09-25 20:36:06 +0500

    Add project files.

---
+++ AuthenticationService/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AuthenticationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        public UserController()
+        {
+            var logger = new Logger();
+
+            logger.WriteEvent("Сообщение о событии в программе");
+            logger.WriteError("Сообщение об ошибки в программе");
+        }
+
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "ivanov@gmail.com",
+                Password = "qwerty123",
+                Login = "IIvanov",
+
+            };
+        }
+    }
+}
---
+++ AuthenticationService/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AuthenticationService
+{
+    public class Logger
+    {
+        public void WriteEvent(string eventMessage) 
+        {
+            Console.WriteLine(eventMessage);
+        }
+
+        public void WriteError(string errorMessage) 
+        {
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
